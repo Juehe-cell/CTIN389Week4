25c57517b03f33ca19b7be601ffc10a58a1bb89c
Isabel <dojofxseito@gmail.com>
2024-09-23 13:29:15 -0700

    Pet and Player characters
    two scripts and two prefabs

---
+++ CTIN 389 Week 4/Assets/Pet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    public GameObject Player;
+    public float TargetDistance;
+    public float AllowedDistance;
+    public GameObject PetNPC;
+    public float followSpeed;
+    public RaycastHit Shot;
+    public string PetState = "follow";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PetState == "follow")
+        {
+            transform.LookAt(Player.transform);
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Shot))
+            {
+                TargetDistance = Shot.distance;
+                //within or out of range
+                if (TargetDistance >= AllowedDistance)
+                {
+                    followSpeed = 0.01f;
+                    //Pet.GetComponent<Animator>().Play("run");
+                    transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, followSpeed);
+                }
+                else //if npc in range, stop moving 
+                {
+                    followSpeed = 0;
+                    //Pet.GetComponent<Animation>().Play("idle");
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("stay put");
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if(TryGetComponent<Player>(out var PlayerScript))
+        {
+
+        }
+    }
+}
